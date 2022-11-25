2449ca56dbf7cd30368c126b49a082aa7b94bebf
Mike Whaley <michael.d.whaley@gmail.com>
2022-11-25 09:18:33 -0500

    comments

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -1,19 +1,31 @@
 // Write your code below this line.
+//create a function for the problem
 function luckNumbers(n) {
+  // declare a number variable and make sure it is a number
   n = Number(n);
+  //create the array
   let luckNumbersArray = [];
+  //create an array to pull numbers from
   let numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  //create an if statement to check n is between 0 and 10
   if (n >= 0 && n <= 10) {
+    //loop n times to get a random number
     for (i = 0; i < n; i++) {
+      //create a variable of a random number between 0 and numArray length
       let a = [Math.floor(Math.random() * numArray.length)];
+      // use that random number as the index of numArray and push that value to lucky
       luckNumbersArray.push(numArray[a]);
+      //splice that number out of numArray so we don't use it again
       numArray.splice(a, 1);
     }
+    //print the new array
     console.log(luckNumbersArray);
   } else {
+    //tell the user to select a valid value
     console.log("Enter a number between 0 and 10");
   }
 }
+//test cases
 luckNumbers(10);
 luckNumbers(9);
 luckNumbers(8);
